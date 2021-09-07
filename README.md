cd13e20fd2c19b554ceab0c32313408a6eee66b5
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-09-07 19:30:32 +0900

    add: base classes & tests

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SCJ_MigdallyWorks
+
 Calculation and Operations for some Migdally Works on SCJ
+
+## How to Use
+
+```sh
+dart migdalDay.dart
+```
+
+for test running:
+
+```sh
+dart migdalDay.dart -test
+```
---
+++ lib/migdalDay.dart
@@ -0,0 +1,87 @@
+void main(List<String> args){
+  if(args.contains("-test")){
+    test_MDD();
+  }else{
+    mainLT();
+  }
+}
+void mainLT(){
+
+  List<List<String>> migdalDaysWithNotes = [
+    ["7/26","エスペラント"],
+    ["6/12","エスペラント(日本)"],
+    ["12/15","ザメンホフ"],
+    ["1/15","インターリングア"]];
+  List<int> migdalCumDays = migdalDaysWithNotes.map((List<String> elm)=>elm[0]).map((String day)=> new DateJP.fromString(day)).map((DateJP date)=>date.cumDays()).toList();
+  int migdalCumDaysMean = (migdalCumDays.reduce((int curr, int next) => curr + next) / migdalCumDays.length).ceil();
+  String migdalDaysMean = (new DateJP.fromCumDays(migdalCumDaysMean)).toString();
+}
+class DateJP{
+  int _month;
+  int _day;
+  List<int> _daysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+  DateJP(int month,int day, [bool isLeap = false]){
+    if(isLeap){
+      this._daysOfMonth[1]=29;
+    }
+    if(month > 0 && month <= 12){
+      this._month = month;
+      if(day > 0 && day <=this._daysOfMonth[month-1]){
+        this._day= day;
+      }else{
+        int val = this._daysOfMonth[this._month-1];
+        throw FormatException("Invalid Imput: Parse failure in the 'day' part | Not in range 1-$val}",day);
+      }
+    }else{
+      throw FormatException("Invalid Imput: Parse failure in the 'month' part | Not in range 1-12",month);
+    }
+  }
+  DateJP.fromString(String dateStr, [bool isLeap = false]){
+    if(isLeap){
+      this._daysOfMonth[1]=29;
+    }
+    if(dateStr.contains("/")){
+      List<int> temp = dateStr.split("/").map((String elm)=> int.parse(elm)).toList();
+      if(temp.length == 2){
+        if(temp[0] > 0 && temp[0] <= 12){
+          this._month = temp[0];
+          if(temp[1] > 0 && temp[1] <=this._daysOfMonth[temp[0]-1]){
+            this._day= temp[1];
+          }else{
+            int val = this._daysOfMonth[this._month-1];
+            throw FormatException("Invalid Imput: Parse failure in the 'day' part | Not in range 1-$val",temp[1]);
+          }
+        }else{
+          throw FormatException("Invalid Imput: Parse failure in the 'month' part | Not in range 1-12",temp[0]);
+        }
+      }else{
+        throw FormatException("Invalid Imput: Parse failure | The number separated by '/' is not two pairs",temp);
+      }
+    }else{
+      throw FormatException("Invalid Imput: Parse failure | '/' does not exist",dateStr);
+    }
+
+  }
+  DateJP.fromCumDays(int cumDays, [bool isLeap = false]){
+    if(isLeap){
+      this._daysOfMonth[1]=29;
+    }
+    int backLen = this._daysOfMonth.map((int days)=>this._daysOfMonth.indexOf(days)).map((int id)=>this._daysOfMonth.take(id)).map((List<int> days)=> days.reduce((int curr, int next) => curr + next)).where((int sum)=>sum >= cumDays).length;
+    int monthID = 12 - backLen;
+    print(monthID+1);
+  }
+  int cumDays(){
+    return this._daysOfMonth.take(this._month).reduce((int curr, int next) => curr + next) + this._day;
+  }
+  @override
+  String toString() {
+    return "${this._month}/${this._day}";
+  }
+}
+
+void test_MDD(){
+  print("This is Test!");
+  DateJP migd = new DateJP.fromString("6/18");
+  DateJP migd2 = new DateJP.fromCumDays(migd.cumDays());
+  print(migd2);
+}
