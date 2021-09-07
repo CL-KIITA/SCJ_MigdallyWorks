a6a9a84e553ff1065593a17ed767c30e9030f9df
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-09-08 00:13:23 +0900

    add: コメント追加、newの削除、DateJP.fromCumDaysの実装中

--- lib/migdalDay.dart
+++ lib/migdalDay.dart
@@ -1,31 +1,42 @@
 void main(List<String> args){
+  //引数からフラグを抽出
   List<String> param = args.where((String arg)=> arg.startsWith("-")).toList();
   if(param.length > 0){
+    //フラグがある場合はフラグごとの挙動
     print("running with ${param.length} params\n\n${param}\n\n");
     if(args.contains("-test")){
+      //-testフラグでtest running
       print("test running\n\n");
       test_MDD();
     }
   }else{
+    //フラグがない場合はメインルーチン
     print("running with no param\n\n");
     mainLT();
   }
 }
 void mainLT(){
-
+  //人工言語関連の言語に関する記念日
   List<List<String>> migdalDaysWithNotes = [
     ["7/26","エスペラント"],
     ["6/12","エスペラント(日本)"],
     ["12/15","ザメンホフ"],
     ["1/15","インターリングア"]];
-  List<int> migdalCumDays = migdalDaysWithNotes.map((List<String> elm)=>elm[0]).map((String day)=> new DateJP.fromString(day)).map((DateJP date)=>date.cumDays()).toList();
+  //累積日数を算出
+  List<int> migdalCumDays = migdalDaysWithNotes.map((List<String> elm)=>elm[0]).map((String day)=> DateJP.fromString(day)).map((DateJP date)=>date.cumDays()).toList();
+  //平均を取り整数に切り上げ
   int migdalCumDaysMean = (migdalCumDays.reduce((int curr, int next) => curr + next) / migdalCumDays.length).ceil();
-  String migdalDaysMean = (new DateJP.fromCumDays(migdalCumDaysMean)).toString();
+  //累積日数からコンストラクトし日付表記の文字列へ
+  String migdalDaysMean = (DateJP.fromCumDays(migdalCumDaysMean)).toString();
 }
+//日本式の日付表記と年内累積日数に関するクラス
+//年内累積日数の定義：n月m日のときn-1月までの日数の和＋m
 class DateJP{
   int _month;
   int _day;
+  //各月の日数。閏年の2月はコンストラクタで変更
   List<int> _daysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+  //月の数値と日の数値からコンストラクト。オプション引数のbool isLeapは閏年かどうか。デフォルトは平年でfalse。閏年の場合はtrueを明示する。
   DateJP(int month,int day, [bool isLeap = false]){
     if(isLeap){
       this._daysOfMonth[1]=29;
@@ -42,6 +53,7 @@ class DateJP{
       throw FormatException("Invalid Imput: Parse failure in the 'month' part | Not in range 1-12",month);
     }
   }
+  //月日の日本式文字列表記からコンストラクト。オプション引数のbool isLeapは閏年かどうか。デフォルトは平年でfalse。閏年の場合はtrueを明示する。
   DateJP.fromString(String dateStr, [bool isLeap = false]){
     if(isLeap){
       this._daysOfMonth[1]=29;
@@ -68,31 +80,41 @@ class DateJP{
     }
 
   }
+  //年内累積日数からコンストラクト。オプション引数のbool isLeapは閏年かどうか。デフォルトは平年でfalse。閏年の場合はtrueを明示する。
   DateJP.fromCumDays(int cumDays, [bool isLeap = false]){
     if(isLeap){
       this._daysOfMonth[1]=29;
     }
-    int backLen = this._daysOfMonth.map((int days)=>this._daysOfMonth.indexOf(days)).map((int id)=>this._daysOfMonth.take(id+1)).map((List<int> days)=> days.reduce((int curr, int next) => curr + next)).where((int sum)=>sum >= cumDays).toList().length;
+    //各月DateJP.fromCumDays
+    int backLen = (List<int>.generate(12, (int x)=>x+1).map((int days)=>this._daysOfMonth.indexOf(days)).map((int id)=>this._daysOfMonth.take(id+1).toList())).map((List<int> days){
+      if(days.length > 0){
+        days.reduce((int curr, int next) => curr + next);
+      }else{
+        return 0;
+      }
+    }).where((int sum)=>sum >= cumDays).toList().length;
     int monthID = 12 - backLen;
-    print(monthID+1);
+    print(backLen);
   }
+  //年内累積日数を算出
   int cumDays(){
-    List<int> backDays = this._daysOfMonth.take(this._month-1);
+    List<int> backDays = this._daysOfMonth.take(this._month-1).toList();
     if(backDays.length > 0){
-    return backDays.reduce((int curr, int next) => curr + next) + this._day;
+      return backDays.reduce((int curr, int next) => curr + next) + this._day;
     }else{
       return this._day;
     }
   }
+  //文字列表記へ
   @override
   String toString() {
     return "${this._month}/${this._day}";
   }
 }
-
+//テスト
 void test_MDD(){
   print("This is Test!\n\n");
-  DateJP migd = new DateJP.fromString("1/1");
-  DateJP migd2 = new DateJP.fromCumDays(migd.cumDays());
-  print(migd);
+  DateJP migd = DateJP.fromString("3/1");
+  DateJP migd2 = DateJP.fromCumDays(migd.cumDays());
+  //print(migd2);
 }
